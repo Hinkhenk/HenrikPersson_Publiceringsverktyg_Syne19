ee76371bbed79d4fdcc9c6bfd71232352777c7c7
Henrik <55479521+Hinkhenk@users.noreply.github.com>
2021-02-03 16:31:02 +0100

    commit

--- student_frontend/gatsby-config.js
+++ student_frontend/gatsby-config.js
@@ -38,7 +38,7 @@ module.exports = {
       },
     },
     {
-      resolve: `gatsby-plugin-prefetch-google-fonts`,
+      resolve: `gatsby-plugin-google-fonts`,
       options: {
         fonts: [
           {
