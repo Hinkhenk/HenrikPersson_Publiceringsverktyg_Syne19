1b33b08a2455da5648e05ad36d3a09148a2607e8
Henrik <55479521+Hinkhenk@users.noreply.github.com>
2021-02-03 17:36:12 +0100

    Update gatsby-config.js

--- student_frontend/gatsby-config.js
+++ student_frontend/gatsby-config.js
@@ -41,13 +41,8 @@ module.exports = {
       resolve: `gatsby-plugin-google-fonts`,
       options: {
         fonts: [
-          {
-            family: `Roboto`,
-            variants: [`400`, `700`],
-          },
-          {
-            family: `Open Sans`,
-          },
+          "Roboto\:400, 700",
+          "Open Sans\:400",
         ],
       },
     },
